5497669c9eccebe5804abc57f4cc0a95c5db42ad
Snigdha Gupta <80680269+snigdha2808@users.noreply.github.com>
2022-05-20 13:06:42 +0530

    Add files via upload

---
+++ cinGetLline.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char s[1000];
+    /* for a sentence of a line
+    cin.getline(s,1000);*/
+
+    /*for a paragraph*/
+    cin.getline(s,1000,'.');
+
+    cout<<s<<endl;
+}
