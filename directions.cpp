bcdd730b5ef003733a73c0ee8e6777510f445d89
Snigdha Gupta <80680269+snigdha2808@users.noreply.github.com>
2022-05-20 13:57:01 +0530

    Add files via upload

---
+++ directions.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char route[1000];
+    cin.getline(route,1000);
+    int x=0,y=0;
+    for(int i=0;route[i]!='\0';i++)
+    {
+        switch(route[i])
+        {
+            case 'N':y++;
+                    break;
+            case 'S':y--;
+                    break;
+            case 'E':x++;
+                    break;
+            case 'W':x--;
+                    break;
+        }
+    }
+    cout<<x<< " "<<y;
+    if(x>=0 && y>=0)
+    {
+        while(y--)
+        cout<<"N";
+        while(x--)
+        cout<<"E";
+    }
+    else if(x>=0 && y<=0)
+    {
+        while(x--)
+        cout<<"E";
+        while(y++)
+        cout<<"S";
+    }
+    else if(x<=0 && y<=0)
+    {
+        while(x++)
+        {
+            cout<<"W";
+        }
+        while(y++)
+        {
+            cout<<"S";
+        }
+    }
+    else
+    {
+        while(x++)
+        cout<<"W";
+        while(y--)
+        cout<<"N";
+    }
+}
